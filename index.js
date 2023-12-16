9e099dfb1ad27e68f6f882d57d0fe2f519a64c5c
Sahavaki2003 <sun15092k3@gmail.com>
2023-12-16 20:46:35 +0700

    refactor code

--- index.js
+++ index.js
@@ -1,185 +1,198 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-const multer = require('multer');
-const session = require('express-session')
-const flash = require('connect-flash')
-const config = require('./config');
-const parseToken = require('./middleware/parseToken');
-const auth = require('./middleware/auth');
-const authorize = require('./middleware/authorize');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const session = require("express-session");
+const flash = require("connect-flash");
+const config = require("./config");
+const parseToken = require("./middleware/parseToken");
+const auth = require("./middleware/auth");
+const authorize = require("./middleware/authorize");
 
-const render = require('./utils/render');
+const render = require("./utils/render");
 
 const storage = multer.diskStorage({
+  // cái này là config đường dẫn đến folder lưu trữ và đọc ảnh ra
+  // muốn thêm ảnh thì phải để vào đây
+  destination: function (req, file, cb) {
+    cb(null, "./public/img");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
 
-	// cái này là config đường dẫn đến folder lưu trữ và đọc ảnh ra
-	// muốn thêm ảnh thì phải để vào đây
-	destination: function (req, file, cb) {
-		cb(null, './public/img')
-	},
-	filename: function (req, file, cb) {
-		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-		cb(null, file.fieldname + '-' + uniqueSuffix)
-	}
-})
+const upload = multer({ storage: storage });
 
-const upload = multer({ storage: storage })
-
-const TODO = require('./model/todo');
-const USER = require('./model/user');
-const BOOK = require('./model/book');
+const TODO = require("./model/todo");
+const USER = require("./model/user");
+const BOOK = require("./model/book");
 
 const app = express();
-app.use(session({
-	secret: 'flashblog',
-	saveUninitialized: true,
-	resave: true
-}))
+app.use(
+  session({
+    secret: "flashblog",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
 app.use(flash());
 
-mongoose.connection.on('connected', () => {
-	console.log('Mongodb connected successful!');
-})
+mongoose.connection.on("connected", () => {
+  console.log("Mongodb connected successful!");
+});
 
-mongoose.connection.on('error', (err) => {
-	console.log(err);
-})
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
 
 mongoose.connect(config.dbURI);
 
-app.set('views', './views');
-app.set('view engine', 'pug');
+app.set("views", "./views");
+app.set("view engine", "pug");
 
 app.use(cookieParser());
 app.use(parseToken);
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', async (req, res) => {
-	const todos = await TODO.find({});
-	console.log('todos', todos);
-	console.log(req.role);
-	// res.render('home', { todos, username: req.username });
-	render(req, res, 'home', { todos });
-})
+app.get("/", async (req, res) => {
+  const todos = await TODO.find({});
+  console.log("todos", todos);
+  console.log(req.role);
+  // res.render("home", { todos, path: req.path });
+  render(req, res, "home", { todos, path: req.path });
+});
 
-app.get('/create-todo', auth, (req, res) => {
-	res.render('create-todo');
-})
+app.get("/create-todo", (req, res) => {
+  render(req, res, "create-todo", { path: req.path });
+});
 
-app.post('/create-todo', async (req, res) => {
-	//validate data
-	console.log('body', req.body);
-	await TODO.create(req.body);
-	console.log("Successfully")
-	res.redirect("/");
-})
+app.post("/create-todo", auth, async (req, res) => {
+  //validate data
+  console.log("body", req.body);
+  await TODO.create(req.body);
+  console.log("Successfully");
+  res.redirect("/");
+});
 
-app.post('/delete-todo/:id', async (req, res) => {
-	await TODO.findByIdAndDelete(req.params.id);
-	res.redirect("/");
-})
+app.post("/delete-todo/:id", auth, async (req, res) => {
+  await TODO.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
 
-app.get('/update-todo/:id', async (req, res) => {
-	const todo = await TODO.findById(req.params.id);
-	res.render('update-todo', { todo });
-})
+app.get("/update-todo/:id", async (req, res) => {
+  const todo = await TODO.findById(req.params.id);
+  render(req, res, "update-todo", { todo, path: req.path });
+});
 
-app.post('/update-todo/:id', async (req, res) => {
-	await TODO.findByIdAndUpdate(req.params.id, req.body);
+app.post("/update-todo/:id", async (req, res) => {
+  await TODO.findByIdAndUpdate(req.params.id, req.body);
 
-	res.redirect("/");
-})
+  res.redirect("/");
+});
 
-app.get('/users', authorize('admin'), async (req, res) => {
-	const users = await USER.find({});
+app.get("/users", authorize("admin"), async (req, res) => {
+  const users = await USER.find({});
 
-	res.render('users', { users });
-})
+  render(req, res, "users", { users, path: req.path });
+});
 
-app.get('/create-user', (req, res) => {
-	res.render('create-user');
-})
+app.get("/create-user", (req, res) => {
+  render(req, res, "create-user", { path: req.path });
+});
 
-app.post('/create-user', async (req, res) => {
-	const salt = crypto.randomBytes(16).toString('hex');
-	const hmac = crypto.createHmac('sha256', salt);
-	const hpass = hmac.update(req.body.password).digest('hex');
+app.post("/create-user", async (req, res) => {
+  const salt = crypto.randomBytes(16).toString("hex");
+  const hmac = crypto.createHmac("sha256", salt);
+  const hpass = hmac.update(req.body.password).digest("hex");
 
-	await USER.create({ ...req.body, salt, hpass });
+  await USER.create({ ...req.body, salt, hpass });
 
-	res.redirect('/users');
-})
+  res.redirect("/users");
+});
 
-app.get('/delete-user/:id', async (req, res) => {
-	await USER.findByIdAndDelete(req.params.id);
-	res.redirect("/users");
-})
+app.get("/delete-user/:id", async (req, res) => {
+  await USER.findByIdAndDelete(req.params.id);
+  res.redirect("/users");
+});
 
-app.get('/update-user/:id', async (req, res) => {
-	const user = await USER.findById(req.params.id);
-	res.render('update-user', { user });
-})
+app.get("/update-user/:id", async (req, res) => {
+  const user = await USER.findById(req.params.id);
+  render(req, res, "update-user", { user, path: req.path });
+});
 
-app.post('/update-user/:id', async (req, res) => {
-	await USER.findByIdAndUpdate(req.params.id, req.body);
+app.post("/update-user/:id", async (req, res) => {
+  await USER.findByIdAndUpdate(req.params.id, req.body);
 
-	res.redirect("/users");
-})
+  res.redirect("/users");
+});
 
-app.get('/books', async (req, res) => {
-	const books = await BOOK.find({});
+app.get("/profile", (req, res) => {
+  // res.render("profile", { path: req.path });
+  render(req, res, "profile", { path: req.path });
+});
 
-	res.render('books', { books });
-})
+app.get("/books", async (req, res) => {
+  const books = await BOOK.find({});
 
-app.get('/create-book', (req, res) => {
-	res.render('create-book');
-})
+  render(req, res, "books", { books, path: req.path });
+});
 
-app.post('/create-book', upload.single('cover'), async (req, res) => {
-	req.body.cover = req.file.filename;
+app.get("/create-book", (req, res) => {
+  render(req, res, "create-book", { path: req.path });
+});
 
-	await BOOK.create(req.body);
+app.post("/create-book", upload.single("cover"), async (req, res) => {
+  req.body.cover = req.file.filename;
 
-	res.redirect("/books");
-})
+  await BOOK.create(req.body);
 
-app.get('/login', (req, res) => {
-	res.render('login');
-})
+  res.redirect("/books");
+});
 
-app.post('/login', async (req, res) => {
-	if (req.body && req.body.username && req.body.password) {
-		const dbUser = await USER.findOne({ username: req.body.username });
-		if (dbUser) {
-			const hmac = crypto.createHmac('sha256', dbUser.salt);
-			const hpass = hmac.update(req.body.password).digest('hex');
-			if (hpass === dbUser.hpass) { //login thanh cong
-				const token = jwt.sign({ username: dbUser.username, role: dbUser.role }, config.secretkey);
-				res.cookie('token', token);
-				res.redirect('/');
-			} else { //password sai
-				// res.end('Wrong password');
-				res.render('login', { errMessage: 'Wrong password' })
-			}
-		} else { //Username khong ton tai
-			//res.end(`${req.body.username} not existed}`);
-			res.render('login', { errMessage: `${req.body.username} not existed}` })
-		}
+app.get("/login", (req, res) => {
+  console.log({ req, res });
+  render(req, res, "login", { path: req.path });
+});
 
-	} else {
-		//res.end('Bad Form Data');
-		res.render('login', { errMessage: 'Bad Form Data' });
-	}
-})
+app.post("/login", async (req, res) => {
+  console.log("body", req.body);
+  if (req.body && req.body.username && req.body.password) {
+    const dbUser = await USER.findOne({ username: req.body.username });
+    if (dbUser) {
+      const hmac = crypto.createHmac("sha256", dbUser.salt);
+      const hpass = hmac.update(req.body.password).digest("hex");
+      if (hpass === dbUser.hpass) {
+        //login thanh cong
+        const token = jwt.sign(
+          { username: dbUser.username, role: dbUser.role },
+          config.secretkey
+        );
+        res.cookie("token", token);
+        res.redirect("/");
+      } else {
+        //password sai
+        // res.end('Wrong password');
+        res.render("login", { errMessage: "Wrong password" });
+      }
+    } else {
+      //Username khong ton tai
+      //res.end(`${req.body.username} not existed}`);
+      res.render("login", { errMessage: `${req.body.username} not existed` });
+    }
+  } else {
+    //res.end('Bad Form Data');
+    res.render("login", { errMessage: "Bad Form Data" });
+  }
+});
 
 app.listen(3000, () => {
-	console.log('App is listening on port 3000');
+  console.log("App is listening on port 3000");
 });
